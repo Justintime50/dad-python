bc96db3127723a7259e5f6e0ed0388840d9dfaae
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-09-10 17:32:52 -0600

    fix: correct references to module

--- test/unit/test_data_router.py
+++ test/unit/test_data_router.py
@@ -1,15 +1,15 @@
-import dad_python
+import dad_tool
 import pytest
 
 
 def test_random_address():
-    address = dad_python.random_address('US_UT')
+    address = dad_tool.random_address('US_UT')
 
     assert address['state'] == 'UT'
 
 
 def test_list_addresses():
-    addresses = dad_python.list_addresses('US_UT')
+    addresses = dad_tool.list_addresses('US_UT')
 
     assert len(addresses) == 100
     assert addresses[0]['state'] == 'UT'
@@ -17,11 +17,11 @@ def test_list_addresses():
 
 def test_bad_data():
     with pytest.raises(KeyError) as error:
-        _ = dad_python.random_address('BAD_DATA')
+        _ = dad_tool.random_address('BAD_DATA')
 
     assert '\'BAD_DATA\'' == str(error.value)
 
 
 def test_list_iso_country_codes():
     with pytest.raises(NotImplementedError):
-        _ = dad_python.list_iso_country_codes()
+        _ = dad_tool.list_iso_country_codes()
