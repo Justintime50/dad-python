2ebf248bfbfb837e53fac654be67569ef6f570c0
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-09-10 17:31:42 -0600

    fix: corrects module folder name

--- dad_python/__init__.py
+++
@@ -1,2 +0,0 @@
-# flake8: noqa
-from dad_python.data_router import list_addresses, list_iso_country_codes, random_address
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ DEV_REQUIREMENTS = [
 
 setuptools.setup(
     name='dad-tool',
-    version='0.1.1',
+    version='0.1.2',
     description='Dummy Address Data (DAD) - Real addresses from all around the world.',
     long_description=long_description,
     long_description_content_type="text/markdown",
