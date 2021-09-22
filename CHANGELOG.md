094a049a143e51092da90b6fa19716f487a455a7
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-09-22 17:24:45 -0600

    chore: release v1.7.0 packaging DAD assets

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 # CHANGELOG
 
-## v0.1.0 - v0.1.6 (2021-09-10/13)
+## v0.1.0 - v0.1.7 (2021-09-10/13/22)
 
 * Initial release allowing you to retrieve addresses from all around the world
--- setup.py
+++ setup.py
@@ -12,21 +12,33 @@ DEV_REQUIREMENTS = [
 
 setuptools.setup(
     name='dad-tool',
-    version='0.1.6',
+    version='0.1.7',
     description='Dummy Address Data (DAD) - Real addresses from all around the world.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url='http://github.com/Justintime50/dad-python',
     author='Justintime50',
     license='MIT',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     extras_require={
         'dev': DEV_REQUIREMENTS,
+    },
+    # TODO: Is there a way we can specify the single `src` dir instead of each individually?
+    package_data={
+        'dad_tool': [
+            'dad/src/addresses/australia/*.json',
+            'dad/src/addresses/canada/*.json',
+            'dad/src/addresses/china/*.json',
+            'dad/src/addresses/europe/*.json',
+            'dad/src/addresses/mexico/*.json',
+            'dad/src/addresses/united-states/*.json',
+            'dad/src/other/*.json',
+        ],
     },
     python_requires='>=3.7',
 )
